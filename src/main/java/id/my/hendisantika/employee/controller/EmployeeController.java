b25131636c1716034f65190cfdde6ca1227850df
hendisantika <hendisantika@yahoo.co.id>
2024-12-24 06:41:30 +0700

    Add EmployeeController🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/employee/controller/EmployeeController.java
@@ -0,0 +1,28 @@
+package id.my.hendisantika.employee.controller;
+
+import id.my.hendisantika.employee.repository.EmployeeRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-crud-reactjs-full-stack-app
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 24/12/24
+ * Time: 06.40
+ * To change this template use File | Settings | File Templates.
+ */
+@Slf4j
+@RestController
+@RequestMapping("/api/v1/employees")
+@RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000")
+public class EmployeeController {
+
+    private final EmployeeRepository employeeRepository;
+}
