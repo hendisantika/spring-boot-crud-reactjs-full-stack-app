b81dfe9e3e6dc09d4fb3fe4d84a3625c80513e85
hendisantika <hendisantika@yahoo.co.id>
2024-12-24 06:39:25 +0700

    Add Employee Model🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/employe/model/Employee.java
@@ -0,0 +1,43 @@
+package id.my.hendisantika.employe.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-crud-reactjs-full-stack-app
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 24/12/24
+ * Time: 06.38
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Table(name = "employees")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email_id")
+    private String emailId;
+}
+
