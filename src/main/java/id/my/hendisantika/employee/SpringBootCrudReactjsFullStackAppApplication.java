da18e942873270a0c71a36d9647bb67434d7fc86
hendisantika <hendisantika@yahoo.co.id>
2024-12-24 07:36:58 +0700

    fix: Error🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/employee/SpringBootCrudReactjsFullStackAppApplication.java
+++ src/main/java/id/my/hendisantika/employee/SpringBootCrudReactjsFullStackAppApplication.java
@@ -1,7 +1,11 @@
 package id.my.hendisantika.employee;
 
+import id.my.hendisantika.employee.model.Employee;
+import id.my.hendisantika.employee.repository.EmployeeRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBootCrudReactjsFullStackAppApplication {
@@ -10,4 +14,14 @@ public class SpringBootCrudReactjsFullStackAppApplication {
         SpringApplication.run(SpringBootCrudReactjsFullStackAppApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner demo(EmployeeRepository employeeRepository) {
+        return (args) -> {
+            employeeRepository.save(new Employee(1L, "Uzumaki", "Naruto", "naruto@yopmail.com"));
+            employeeRepository.save(new Employee(2L, "Uchiha", "Sasuke", "sasuke@yopmail.com"));
+            employeeRepository.save(new Employee(3L, "Haruno", "Sakura", "sakura@yopmail.com"));
+            employeeRepository.save(new Employee(4L, "Hatake", "Kakashi", "kakashi@yopmail.com"));
+            employeeRepository.save(new Employee(5L, "Itadori", "Yuji", "yuji@yopmail.com"));
+        };
+    }
 }
--- src/main/java/id/my/hendisantika/employee/model/Employee.java
+++ src/main/java/id/my/hendisantika/employee/model/Employee.java
@@ -37,7 +37,7 @@ public class Employee {
     @Column(name = "last_name")
     private String lastName;
 
-    @Column(name = "email_id")
-    private String emailId;
+    @Column(name = "email", unique = true)
+    private String email;
 }
 
